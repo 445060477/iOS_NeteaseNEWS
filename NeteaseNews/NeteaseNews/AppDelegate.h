de70a565dbaef8ccf13e994af11616bf15f1ffb3
445060477 <445060477@qq.com>
2016-04-15 01:46:35 +0800

    初始化

---
+++ NeteaseNews/NeteaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeteaseNews
+//
+//  Created by yws on 16/4/15.
+//  Copyright © 2016年 yws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by yws on 16/4/15.
+//  Copyright © 2016年 yws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
